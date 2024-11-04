263a813476342f6a83cf65fc498ab8f1438e5459
Ludvigpbf <ludvig.flyckt@gmail.com>
2024-11-04 21:51:30 +0100

    Bump version for npm publish

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import './styles/index.css';
+
 export { default as Dots } from './loaders/dots-loader/Dots';
 export { default as Spinner } from './loaders/spinner-loader/Spinner';
 export { default as InfiniteBar} from './loaders/infinite-bar-loader/InfiniteBar';
